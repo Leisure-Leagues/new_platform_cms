24f80e0bbf7ae034c9901bf7532d06c1573907a0
Daniel Earp <danielearp@leisureleagues.net>
2023-12-13 12:58:34 +0000

    Added profile page to view your own user information

---
+++ src/pages/profile/ProfileTimeline.js
@@ -0,0 +1,177 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import { styled } from '@mui/material/styles'
+import TimelineDot from '@mui/lab/TimelineDot'
+import TimelineItem from '@mui/lab/TimelineItem'
+import CardHeader from '@mui/material/CardHeader'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import MuiTimeline from '@mui/lab/Timeline'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// Styled Timeline component
+const Timeline = styled(MuiTimeline)({
+  paddingLeft: 0,
+  paddingRight: 0,
+  '& .MuiTimelineItem-root': {
+    width: '100%',
+    '&:before': {
+      display: 'none'
+    }
+  }
+})
+
+const ProfileTimeline = () => {
+  return (
+    <Card>
+      <CardHeader
+        title='Activity Timeline'
+        sx={{ '& .MuiCardHeader-avatar': { mr: 2.5 } }}
+        avatar={<Icon icon='mdi:chart-timeline-variant' />}
+        titleTypographyProps={{ sx: { color: 'text.primary' } }}
+      />
+      <CardContent>
+        <Timeline sx={{ my: 0, py: 0 }}>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color='error' />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2.75)} !important` }}>
+              <Box
+                sx={{
+                  mb: 2.5,
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography sx={{ mr: 2, fontWeight: 600 }}>Updated Password</Typography>
+                <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                  Just now
+                </Typography>
+              </Box>
+              <Typography variant='body2' sx={{ mb: 2 }}>
+                Daniel Earp updated his password details
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color='info' />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2.75)} !important` }}>
+              <Box
+                sx={{
+                  mb: 2.5,
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography sx={{ mr: 2, fontWeight: 600 }}>Fixtures Generated</Typography>
+                <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                  Wednesday
+                </Typography>
+              </Box>
+              <Typography variant='body2' sx={{ mb: 2 }}>
+                Daniel Earp generated a new set of fixtures for Walsall Monday
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color='info' />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2.75)} !important` }}>
+              <Box
+                sx={{
+                  mb: 2.5,
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography sx={{ mr: 2, fontWeight: 600 }}>Venue Details updated</Typography>
+                <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                  April, 18
+                </Typography>
+              </Box>
+              <Typography variant='body2' sx={{ mb: 2.5 }}>
+                Daniel Earp updated the venue address of Coventry Monday to{' '}
+                <b>Coventry Sports Center, Coventry Road, Coventry, CV1 8QQ</b>
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color='info' />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2.75)} !important` }}>
+              <Box
+                sx={{
+                  mb: 2.5,
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography sx={{ mr: 2, fontWeight: 600 }}>New Season created</Typography>
+                <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                  January, 19
+                </Typography>
+              </Box>
+              <Typography variant='body2' sx={{ mb: 2 }}>
+                Daniel Earp created a new season at Birmingham Monday
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color='success' />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ mt: 0, mb: theme => `${theme.spacing(2.75)} !important` }}>
+              <Box
+                sx={{
+                  mb: 2.5,
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography sx={{ mr: 2, fontWeight: 600 }}>Account Creation</Typography>
+                <Typography variant='caption' sx={{ color: 'text.disabled' }}>
+                  January 1st
+                </Typography>
+              </Box>
+              <Typography variant='body2' sx={{ mb: 2 }}>
+                Account Created
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default ProfileTimeline
